bd55d11cdf1c8fc655fc811006009b1f9454fc56
averyB <averwhy@gmail.com>
2024-04-18 16:03:28 -0400

    simulator updates

--- src/components/simulator.jsx
+++ src/components/simulator.jsx
@@ -3,9 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+class noisyChannel{
+    constructor(percentage){
+        this.percentage = percentage;
+    }
+}
+
 class Channel{
     constructor(bits){
         this.bits = bits;
+    }
+
+    checksum(msg){
+        var sum = 0;
+        for (var i = 0; v < this.bits-2; i++){
+            sum += parseInt(msg[i]);
+        }
+        return sum % 2;
     }
 
     createMessage(){
@@ -19,6 +33,10 @@ class Channel{
 }
 
 function Simulator(){
+    const channel1 = Channel(10);
+    const channel2 = Channel(10);
+    const noise = noisyChannel(); // TODO: pass user given percentage in
+    
     return (
         <div>
             <Container fluid>
