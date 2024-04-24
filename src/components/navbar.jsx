265418f7537ed2e06e3099d56c9c17f11cece2a4
averyB <averwhy@gmail.com>
2024-04-24 04:45:03 -0400

    final tweaks to about n stuff

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,6 +12,7 @@ function NoisyNavBar() {
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
+            <Navbar.Text style={{position: 'relative', left: '1090px'}}>v1.0.0</Navbar.Text>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -26,14 +26,16 @@ function About(){
                         <Col>MAT239, Prof. Gilbert</Col>
                     </Row>
                 </Container>
-                <br style={{display: 'block', margin:'50px 0'}}/> {/*i HAVE to style this because it wont work otherwise*/}
+                <br style={{display: 'block', margin:'5px 0'}}/> {/*i HAVE to style this because it wont work otherwise*/}
                 <Card bg='dark' text='light' style={{width: '20rem'}}>
                     <Card.Body>
                         <Card.Img variant="top" src="https://opengraph.githubassets.com/47e3b37c90e7657dcca45a3799d460b71f5621b1114bdbfe0cce7824c82561eb/averwhy/noisyProject/100px180"/>
-                        <Card.Text>Source Code</Card.Text>
-                        <Card.Title> {/* really dont know why i have to swap these but */}
-                            The source code to the website can be found here. It uses the MIT license.
+                        <Card.Title>
+                            Source Code
                         </Card.Title>
+                        <Card.Text style={{fontSize: '16px'}}>
+                        The source code to this website can be found here. It uses the MIT license.
+                        </Card.Text>
                         <Button variant="primary" onClick={() => window.open(source, '_blank')}>Check it out</Button>
                     </Card.Body>
                 </Card>
